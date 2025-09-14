33d199a555568be083bdf66ce5ee0e5bbf4c5ca8
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-09-14 20:07:58 +0530

    Update Inorder Traversal.py

--- Binary Tree/Inorder Traversal.py
+++ Binary Tree/Inorder Traversal.py
@@ -1,1 +1,16 @@
+def inorder_traversal(node, arr):
+    if node is None:
+        return
+    inorder_traversal(node.left, arr)
+    arr.append(node.val)
+    inorder_traversal(node.right,arr)
+    
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
 
+result = []
+inorder_traversal(root, result)
+print("Inorder Traversal:", result)
